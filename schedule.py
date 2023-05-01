b39ba2da8b4526fde0131a9cf6970c61f214ffd5
gozdeaygun <81327363+gozdeaygun@users.noreply.github.com>
2023-05-01 21:52:50 +0300

    Optimization model is implemented to the simulation

--- schedule.py
+++ schedule.py
@@ -1,4 +1,5 @@
 from order import Order
+import gurobipy as gp
 
 class JobQueue(object):
 
@@ -29,14 +30,47 @@ class JobQueue(object):
         return order
         
     def reschedule(self, due_date_params=True) -> dict:
-#         if self._policy == 'optimization':
-#             b, p, d = [], [], []
-#             for order in self._orders_unordered:
-#                 b.append(order.get_tardiness_cost())
-#                 p.append(order.get_expected_process_time())
-#         # solve model        
-#         self._proposed_new_schedule = np.argsort(C)
-        
+        if self._policy == 'optimization':
+            model = gp.Model('opt_model')
+            n = len(self._orders_unordered)
+            a, b, p, d = [],[],[],[]
+            for order in self._orders_unordered:
+                a.append(order._weight*0.8)
+                p.append(order._weight)
+                b.append(order._expected_process_time)
+                d.append(order._due_date)
+            I = range(n)
+            M= sum(p) + 1
+            L= [(i,j) for i in I for j in range(i+1,n)]
+            C = model.addVars(I, lb=0, vtype=gp.GRB.INTEGER, name='C')  # completion time of each job
+            T = model.addVars(I, lb=0, vtype=gp.GRB.INTEGER, name='T')  # tardiness for each job
+            Y = model.addVars(L,lb=0, vtype=gp.GRB.BINARY, name='Y')  # binary variables for job sequencing
+   
+            model.setObjective(a[n-1] * C[n-1] + gp.quicksum(b[i] * T[i] for i in range(n-1)), sense=gp.GRB.MINIMIZE)
+    
+            for i in I:
+            for j in range(i+1, n):
+                print(i,j)
+                model.addConstr(C[i] <= C[j] - p[j] + M * (1 - Y[i,j]))
+                model.addConstr(C[j] <= C[i] - p[i] + M * (Y[i,j]))
+
+            for i in range(n-1):
+                model.addConstr(T[i] >= C[i] - d[i])
+                model.addConstr(T[i] >= 0)
+
+            for i in I:
+                model.addConstr(C[i] >= p[i])
+    
+            model.optimize()
+            c=[]
+            if model.status == gp.GRB.OPTIMAL:
+                for i in range(n):
+                c.append(C[i].x)
+                self._proposed_new_schedule = list(np.array(self._orders_unordered)[np.argsort(c)])
+    
+    
+    
+    
         if self._policy != 'optimization':
             self._proposed_new_schedule = sorted(self._orders_unordered, reverse=True)
         if not due_date_params:
